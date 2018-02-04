4f66d65058ccee18d8973f7b8f5cd0da18d422e3
Jordan Boggs <boggsjp@gmail.com>
2018-02-04 14:05:56 -0700

    Menu options

--- bamazonManager.js
+++ bamazonManager.js
@@ -8,3 +8,19 @@ const connection = mysql.createConnection({
   database : 'bamazon',
   port     : 8889
 });
+
+connection.query("SELECT * FROM products", function(err, res) {
+  if (err) console.log(colors.red(err));
+
+  inquirer.prompt([
+    {
+      type: list,
+      name: action,
+      message: "Choose an action:",
+      choices: ["View products for sale",
+                "View low inventory",
+                "Add to inventory",
+                "Add new product"]
+    }
+  ]).then();
+});
