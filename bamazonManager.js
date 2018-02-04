da6ca6a009cfbd8b854d2c0ed41a8b5d7b6a9517
Jordan Boggs <boggsjp@gmail.com>
2018-02-04 14:16:32 -0700

    viewProducts function

--- bamazonManager.js
+++ bamazonManager.js
@@ -9,18 +9,40 @@ const connection = mysql.createConnection({
   port     : 8889
 });
 
-connection.query("SELECT * FROM products", function(err, res) {
-  if (err) console.log(colors.red(err));
-
-  inquirer.prompt([
-    {
-      type: list,
-      name: action,
-      message: "Choose an action:",
-      choices: ["View products for sale",
-                "View low inventory",
-                "Add to inventory",
-                "Add new product"]
-    }
-  ]).then();
+// Prompt manager for action
+inquirer.prompt([
+  {
+    type: "list",
+    name: "action",
+    message: "Choose an action:",
+    choices: ["View products for sale",
+              "View low inventory",
+              "Add to inventory",
+              "Add new product"]
+  }
+]).then(function(answer) {
+  switch (answer.action) {
+    case ("View products for sale"):
+      viewProducts();
+      break;
+  }
 });
+
+// list every available item: the item IDs, names, prices, and quantities
+const viewProducts = function() {
+  let query = "SELECT id, product_name, price, stock_quantity FROM products";
+  connection.query(query, function(err, res) {
+    if (err) {
+      console.log(colors.red(err));
+    }
+    else {
+      for (let i = 0; i < res.length; i++) {
+        console.log(
+          `ID: ${res[i].id} Product: ${res[i].product_name} Price: $` + 
+          `${res[i].price} Inventory: ${res[i].stock_quantity}`
+        );
+      }
+    }
+  });
+  connection.end();
+}
