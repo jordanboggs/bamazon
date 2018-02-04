83215c30c217f31f0b5d0830f27fd47c50a8f5ee
Jordan Boggs <boggsjp@gmail.com>
2018-02-03 18:10:14 -0700

    Can now match choice to index

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,21 +6,21 @@ const connection = mysql.createConnection({
   user     : 'root',
   password : 'root',
   database : 'bamazon',
-  port: 8889
+  port     : 8889
 });
 
 // connection.query('QUERY ?', [values], function(err, res))
 
 // Prompt customer to pick an item to buy
 connection.query("SELECT * FROM products", function(err, res) {
-  if (err) console.log(err);
+  if (err) console.log(colors.red(err));
 
   let products = [];
   let printProducts = [];
   for (let i = 0; i < res.length; i++) {
     products.push(res[i]);
     printProducts.push(
-      `ID: ${colors.white(res[i].item_id)}  Product: ${res[i].product_name}  Price: ${colors.blue(res[i].price)}`
+      `ID: ${res[i].item_id}  Product: ${res[i].product_name}  Price: ${res[i].price}`
     );
   }
   inquirer.prompt([
@@ -36,15 +36,30 @@ connection.query("SELECT * FROM products", function(err, res) {
       message: "How many would you like to buy?"
     }
   ]).then(answers => {
-    const product = answers.productId;
+    const productPrintout = answers.productId;
+    console.log("productPrintout",productPrintout);
     const desiredQuant = answers.desiredQuant; 
+
+    const productIndex = findIndexOfProduct(printProducts, productPrintout);
+    console.log("the index is",productIndex);
     
-    if (desiredQuant <= product.stock_quantity) {
-      // reduce the stock_quantity by desiredQuant
-    }
-    else {
-      console.log(colors.yellow("Sorry, we only have " + product.stock_quantity 
-        + " in stock"));
-    }
+    // if (desiredQuant <= product.stock_quantity) {
+    //   // reduce the stock_quantity on DB by desiredQuant
+    // }
+    // else {
+    //   // log Sorry, we only have <quant> in stock
+    // }
   });
+  connection.end();
 });
+
+const findIndexOfProduct = function(printProducts, productPrintout) {
+  // Match productPrintout to appropriate index of printProducts array
+  // then use that index to match it to the products array
+  for (let i = 0; i < printProducts.length; i++) {
+    if (printProducts[i].match(productPrintout)) {
+      return i;
+    }
+  }
+  return -1;
+};
